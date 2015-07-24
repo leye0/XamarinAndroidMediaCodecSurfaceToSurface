86904d5b6b1380727293eded1f0367ae0ae6fcc3
Léon Pelletier <leon.pelletier@apcurium.com>
2015-07-24 02:40:36 -0400

    Xamarin Android MediaCodec Surface To Surface - CTS ported to Xamarin Android C#

---
+++ surfacetosurface/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MediaCodecHelper;
+using System.Threading.Tasks;
+
+namespace surfacetosurface
+{
+	[Activity (Label = "surfacetosurface", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			var myCodec = new MediaCodecWrapper (this);
+			button.Click += delegate {
+				Task.Run(() => {
+					myCodec.StartTest();	
+				});
+			};
+		}
+	}
+}
+
+
